5ac878404900b31f8fec942f42d3321fd95f4d78
silverhinno <silverhinno@gmail.com>
2021-06-07 20:16:04 +0300

    video #37

---
+++ EmployeeManagement.Web/Models/EditEmployeeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models.CustomValidators;
+using System.ComponentModel.DataAnnotations;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Web.Models
+{
+    public class EditEmployeeModel
+    {
+        public int EmployeeId { get; set; }
+        [Required]
+        [MinLength(2)]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [EmailAddress]
+        [EmailDomainValidator(AllowedDomain = "pragimtech.com")]
+        public string Email { get; set; }
+        [CompareProperty("Email", ErrorMessage = "Email and Confirm Email must match")]
+        public string ConfirmEmail { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public int DepartmentId { get; set; }
+        public string PhotoPath { get; set; }
+        public Department Department { get; set; }
+    }
+}
--- EmployeeManagement.Web/Pages/EditEmployeeBase.cs
+++ EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 using System;
@@ -13,7 +14,9 @@ namespace EmployeeManagement.Web.Pages
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
 
-        public Employee Employee { get; set; } = new Employee();
+        private Employee Employee { get; set; } = new Employee();
+
+        public EditEmployeeModel EditEmployeeModel { get; set; } = new EditEmployeeModel();
 
         [Inject]
         public IDepartmentService DepartmentService { get; set; }
@@ -27,7 +30,19 @@ namespace EmployeeManagement.Web.Pages
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
             Departments = (await DepartmentService.GetDepartments()).ToList();
-            
+
+            EditEmployeeModel.EmployeeId = Employee.EmployeeId;
+            EditEmployeeModel.FirstName = Employee.FirstName;
+            EditEmployeeModel.LastName = Employee.LastName;
+            EditEmployeeModel.Email = Employee.Email;
+            EditEmployeeModel.ConfirmEmail = Employee.Email;
+            EditEmployeeModel.DateOfBirth = Employee.DateOfBirth;
+            EditEmployeeModel.Gender = Employee.Gender;
+            EditEmployeeModel.PhotoPath = Employee.PhotoPath;
+            EditEmployeeModel.DepartmentId = Employee.DepartmentId;
+            EditEmployeeModel.Department = Employee.Department;
         }
+        protected void HandleValidSubmit()
+        { }
     }
 }
